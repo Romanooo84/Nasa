2b2c64e132deacfcdc764fe56a750315da989f0e
Pisarski Roman <pisarski.roman@gmail.com>
2024-10-11 11:59:28 +0200

    styled and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,48 @@
 
 import { useEffect, useState } from 'react';
-import { token } from './data/code.ts'
+import { fetchPicture } from './hoocks/download.ts';
+import { Image, Text, Flex, Heading } from "@chakra-ui/react"
 
 interface NasaPictureData {
   url: string;
+  title: string;
+  explanation: string;
+  hdurl: string;
 }
 
 function App() {
+  const [pictures, setPictures] = useState<NasaPictureData[]>([]);
+  const [pictureRender, setPictureRender] = useState<JSX.Element[]>([]); // Updated to store JSX elements
 
-  const [picture, setPicture]=useState<string | undefined>(undefined)
-
-   useEffect(() => { 
-    const fetchPicture = async () => { 
-      const url = `https://api.nasa.gov/planetary/apod?api_key=${token}`;
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const downloadedData: NasaPictureData = await response.json();
-        setPicture(downloadedData.url); 
-      } catch (error) {
-        console.error("Data error:", error);
-      }
+  useEffect(() => { 
+    const fetchData = async () => {
+      const downloadedPictures: NasaPictureData[]  = await fetchPicture(); // Ensure this is returning an array of NasaPictureData
+      setPictures(downloadedPictures);
     };
+    fetchData();
+  }, []);
 
-    fetchPicture(); 
-  }, [])
-  
-
-  
+  useEffect(() => {
+    if (pictures.length > 0) {
+      const render = pictures.map((picture, index) => (
+        <Flex display="flex" key={index}>
+          <Flex display="flex" flexDirection="column">
+            <Heading  as='h3' width='400px'>{picture.title}</Heading>
+            <Text width='400px'>{picture.explanation}</Text>
+          </Flex>
+          <Image src={picture.hdurl} alt={picture.title} width='400px'/>
+        </Flex>
+      ));
+      setPictureRender(render); // Set the rendered JSX elements
+    }
+  }, [pictures]);
 
   return (
-  <div>
-      <p>Nasa Project try to start and delete gh-pages</p>
-      <img src={picture}></img>
+    <div>
+      <Heading  as='h1' width='400px'>Do you know that...?</Heading>
+      {pictureRender.length > 0 ? pictureRender : <p>Loading...</p>}
     </div> 
-  )
+  );
 }
 
-export default App
+export default App;
