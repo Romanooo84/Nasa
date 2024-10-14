e77706dee86dad7a54b0a20196d0c5b3dd6b04b5
Pisarski Roman <pisarski.roman@gmail.com>
2024-10-14 09:15:41 +0200

    interface gallery

--- src/components/gallery/Render.tsx
+++ src/components/gallery/Render.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { Image, Button, Text } from "@chakra-ui/react";
+import { FaImage } from "react-icons/fa"
+import { FaVideo } from "react-icons/fa6";
+import { MdAudiotrack } from "react-icons/md";
 import { GalleryData } from "./GalleryData";
 
 interface GalleryRenderProps {
-    gallery: GalleryData | null; // Użyj jednolitego typu
-    onButtonClick: (href: string) => void; // Nasa ID do przekazania w onClick
+    gallery: GalleryData | null; 
+    onButtonClick: (href: string) => void; 
 }
 
 export const GalleryRender = ({ gallery, onButtonClick }: GalleryRenderProps) => {
@@ -13,31 +16,39 @@ export const GalleryRender = ({ gallery, onButtonClick }: GalleryRenderProps) =>
     useEffect(() => {
         if (gallery?.collection) {
             const galleryMarkup = gallery.collection.items.map((item) => {
-                // Sprawdź, czy są dostępne linki i dane
                 if (item.links && item.links.length > 0 && item.data && item.data.length > 0) {
                     return (
                         <Button
-                            onClick={() => onButtonClick(item.href)} // Użyj href z item
-                            key={item.href} // Użyj item.href jako klucz
+                            onClick={() => onButtonClick(item.href)} 
+                            key={item.href} 
+                            name={item.data[0].media_type}
                             height='400px'
                             width='400px'
                             flexWrap='wrap'>
                             <Image 
                                 alt={item.data[0].title} 
-                                src={item.links[0].href} // Link do obrazu
-                                width='350px' 
+                                src={item.links[0].href} 
+                                width='300px' 
+                                height='300px'
                             />
                             <Text>{item.data[0].title}</Text>
+                            {item.data[0].media_type === 'video' ? (
+                                <Text><FaVideo /></Text>
+                            ) : item.data[0].media_type === 'image' ? (
+                                 <Text><FaImage /></Text>
+                            ) : (
+                                 <Text><MdAudiotrack /></Text>
+                            )}
                         </Button>
                     );
                 }
-                return null; // Unikaj błędów, gdy brak danych
+                return null; 
             });
             setRender(galleryMarkup.filter((item): item is JSX.Element => item !== null));
         }
     }, [gallery]);
 
     return (
-        <>{render}</> // Zwróć JSX
+        <>{render}</> 
     );
 };
