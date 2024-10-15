5c0fe0b9362b49a5d8b1672b6d55be8be4033fea
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-10-15 17:28:52 +0200

    location

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import { Box, Button } from "@chakra-ui/react"
 import { useEffect, useState } from "react";
 import { SiNasa } from "react-icons/si";
-import { Link, useLocation  } from "react-router-dom"
+import { Link} from "react-router-dom"
 
 export const Header = () => {
 
