095239514524312fae9ce3b88e7491796221e183
Pisarski Roman <pisarski.roman@gmail.com>
2024-10-15 15:53:23 +0200

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
 import PictureOfADay from "./components/PictureOfADay/PictureOfADay";
 import Gallery from "./components/gallery/Gallery";
-import { Box, Flex } from "@chakra-ui/react"
-import backgroundImage from './media/GSFC_20171208_Archive_e000118~large.jpg'
+import SharedLayout from "./components/sharedLayout/SharedLayout";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+
 
 function App() {
 
   return (
-     <Box>
-            <Box 
-                position="absolute"
-                top="0"
-                left="0"
-                right="0"
-                bottom="0"
-                backgroundImage={`url(${backgroundImage})`}
-                backgroundSize="cover"
-                backgroundPosition="center"
-                backgroundRepeat="no-repeat"
-                backgroundAttachment="fixed"
-                zIndex="-1"
-            />
-              <Flex 
-                justifyContent="space-evenly" 
-                wrap="wrap" 
-                m="0 auto" 
-                w="80vw "
-              >
-                <Gallery />
-                <PictureOfADay />
-            </Flex>
-        </Box>
-  );
+    <Routes>
+      <Route path="/Nasa/" element={<SharedLayout />}>
+        <Route path="/Nasa/pictureofaday" element={<PictureOfADay />}></Route>
+        <Route path="/Nasa/gallery" element={<Gallery/>}></Route>
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
